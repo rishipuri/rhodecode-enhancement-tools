7ef8f087845627df8f0f0b66d504c078bca7ca57
rishipuri <rishipuri@live.in>
2017-09-05 11:40:47 +0100

    Inject script into page if url is equal to options url

--- extension/background.js
+++ extension/background.js
@@ -1,1 +1,26 @@
-console.log('Hello World!')
+let url = ''
+
+function init() {
+  chrome.storage.sync.get({
+    url: ''
+  }, function(items) {
+    url = items.url
+
+    injectScriptIfUrlIsValid(url)
+  })
+}
+
+function injectScriptIfUrlIsValid(url) {
+  chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (tab.status === "complete" && tab.url.indexOf(url) === 0) {
+      console.log(url)
+
+      chrome.tabs.executeScript(tabId, {
+        file: 'content.js',
+        runAt: 'document_start'
+      })
+    }
+  })
+}
+
+init()
